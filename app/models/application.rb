89c87be601343e1731c685693deb3d0f778cb381
Maxence Chauvin <maxence.chauvin@edhec.com>
2021-06-22 11:07:13 +0200

    Models

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,10 @@
 class Application < ApplicationRecord
   belongs_to :user
   belongs_to :organization
+  has_many :contacts, through: :application_record
+  has_many :interviews
+  has_many :reviews, through: :interview
+  validates :job_title, presence: true
+  validates :description, presence: true
+  validates :status, presence: true
 end
