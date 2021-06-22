8fd7e1cdd36a05fcb5342542103b423241182074
Maxence Chauvin <maxence.chauvin@edhec.com>
2021-06-22 14:41:14 +0200

    Controllers

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,40 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:destroy, :edit, :update]
+
+  def create
+    @contact = Contact.new(contact_params)
+    @application = Application.find(params[:id])
+    @contact.applications = @applications
+    result = @applications.filter { |application| application == @application }
+    result.user = current_user
+    @contact.save
+    redirect_to application_path(result)
+    authorize @contact
+  end
+
+  def edit
+    authorize @contact
+  end
+
+  def update
+    @contact.update(contact_params)
+    redirect_to application_path(result)
+    authorize @contact
+  end
+
+  def destroy
+    @contact.destroy
+    redirect_to application_path(result)
+    authorize @contact
+  end
+
+  private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:name, :linkedin_url)
+  end
 end
