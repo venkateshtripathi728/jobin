d6ca5b0778536f93c8cc02a785c180d65a4902a3
venkatesh tripathi <venkatesh.tripathi@edhec.com>
2021-06-22 11:02:15 +0200

    newjobinfeature

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,8 @@
 class Interview < ApplicationRecord
   belongs_to :application
+  has_one :review
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :step, presence: true
+  validates :notes, presence: true
 end
