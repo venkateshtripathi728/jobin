428f8a6eaf2bf24f575862e77d02a44986b4febe
Maxence Chauvin <maxence.chauvin@edhec.com>
2021-06-22 16:21:18 +0200

    Contact controller

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,8 @@
 class Application < ApplicationRecord
   belongs_to :user
   belongs_to :organization
-  has_many :contacts, through: :application_record
+  has_many :contact_applications
+  has_many :contacts, through: :contact_applications, dependent: :destroy
   has_many :interviews
   has_many :reviews, through: :interview
   validates :job_title, presence: true
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :organization
-  has_many :processes, through: :application_record
+  has_many :contact_applications
+  has_many :applications, through: :contact_applications, dependent: :destroy
   has_one_attached :photo
-  validates :name, :presence => true, :if => "linkedin_url.blank?"
-  validates :photo, :presence => true, :if => "linkedin_url.blank?"
+  validates :name, presence: true, unless: :linkedin_url
 end
