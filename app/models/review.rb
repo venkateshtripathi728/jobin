ec9971391825f8e2d06ceb9a1ec67072c7150613
Thomas Dugue <thomas.dugue@edhec.com>
2021-06-23 16:46:21 +0200

    6

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :interview
   has_many :votes
-  validates :comment, presence: true,length: {minimum :6}
+  validates :comment, presence: true,length: {minimum: 6}
   validates :rating, presence: true
 end
